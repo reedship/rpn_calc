4d3218b2e2215e6d031ed635689b70b9005dcd3a
Reed Shipley <reedbshipley@gmail.com>
2022-09-30 12:45:47 -0600

    Basic implemenation without test cases

---
+++ Calc.rb
@@ -0,0 +1,41 @@
+class Calc
+  def initialize(arr)
+    @stack = []
+    @arr = arr.contains_string? ? arr.convert_to_i_array : arr
+  end
+
+  def eval()
+    # if we are only passed one item, then return the item passed in
+    if arr.length == 1
+      return arr.first
+    end
+
+    arr.each do |i|
+      # if the item is a number, add it to the stack
+      if i.match(/[0-9]/) != nil
+        @stack.push(i)
+      # for each operator, the only items we need to evaluate are the first two in the stack
+      # calling to_sym return the operator string as a symbol that can be passed to the reduce function
+      # IE:
+      # '+' => :+
+      # '-' => :-
+      # REF: https://apidock.com/ruby/Enumerable/reduce
+      else
+        @stack.push(@stack.pop(2).reduce(i.to_sym)
+      end
+    end
+    return @stack.first
+  end
+
+  private
+
+  # storing these two helper methods as private as they will only be called in the constructor
+  def contains_string?(self)
+    self.any? {|i| i.instance_of?(String) }
+  end
+
+  def convert_to_i_array(arr)
+    # apply the method to_i to each item in the array
+    arr.map(&:to_i)
+  end
+end
